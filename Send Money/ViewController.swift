f35931a28f7c35d23890e85125f18f570974a419
falconevents <pratheeshdhayadev@gmail.com>
2025-01-29 16:16:30 +0400

    first commit

---
+++ Send Money/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Send Money
+//
+//  Created by Pratheesh Bennet on 29/01/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
---
+++ Send MoneyTests/Send_MoneyTests.swift
@@ -0,0 +1,17 @@
+//
+//  Send_MoneyTests.swift
+//  Send MoneyTests
+//
+//  Created by Pratheesh Bennet on 29/01/25.
+//
+
+import Testing
+@testable import Send_Money
+
+struct Send_MoneyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
